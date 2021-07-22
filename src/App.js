d5511d0a1da64fe167912cc4c6d474bb3f45340f
Vishal <trickyjs091991@gmail.com>
2021-07-22 16:37:47 +0530

    currency convertor using react hooks

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Convertor from "./CurrencyConvertor";
+
+function App() {
+  return (
+    <div className="App">
+      <Convertor />
+    </div>
+  );
+}
+
+export default App;
